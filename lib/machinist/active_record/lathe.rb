e1adc4b1dc881360a7192ada686df78f7b6a5d53
Chris Mear <chris@feedmechocolate.com>
2012-04-09 12:55:09 +0100

    When 'make'ing an association object, don't overwrite the reference to the parent.

--- lib/machinist/active_record/lathe.rb
+++ lib/machinist/active_record/lathe.rb
@@ -19,6 +19,23 @@ module Machinist::ActiveRecord
         raise_argument_error(attribute)
       end
     end
+    
+    def method_missing(attribute, *args, &block) #:nodoc:
+      # Don't overwrite existing associations that the
+      # object has been instantiated with.
+      if object.class.reflect_on_association(attribute)
+        association_object = (object.send(attribute))
+        if association_object.respond_to?(:empty?)
+          return unless association_object.empty?
+        else
+          return unless association_object.nil?
+        end
+      end
+        
+      unless attribute_assigned?(attribute)
+        assign_attribute(attribute, make_attribute(attribute, args, &block))
+      end
+    end
 
   end
 end
